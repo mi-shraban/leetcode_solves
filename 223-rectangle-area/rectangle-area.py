e60b7003e7a0674d77aa4c172f21191047115581
Md Monowarul Islam <83732674+mi-shraban@users.noreply.github.com>
2025-09-22 14:44:42 -0400

    Time: 7 ms (12.77%) | Memory: 17.9 MB (37.04%) - LeetSync

---
+++ 223-rectangle-area/rectangle-area.py
@@ -0,0 +1,6 @@
+class Solution:
+    def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
+        area_a = (ax2-ax1) * (ay2-ay1)
+        area_b = (bx2-bx1) * (by2-by1)
+        area_o = max(0, min(ax2, bx2) - max(ax1, bx1)) * max(0, min(ay2, by2) - max(ay1, by1))
+        return area_a + area_b - area_o        
