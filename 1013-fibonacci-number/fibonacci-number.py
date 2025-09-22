b0830f8c10f9bf1206568a5859f90983b41ed51d
Md Monowarul Islam <83732674+mi-shraban@users.noreply.github.com>
2025-09-22 15:20:25 -0400

    Time: 33 ms (87.10%) | Memory: 17.7 MB (63.90%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -2,9 +2,9 @@ class Solution:
     def fib(self, n: int) -> int:
         if n == 0 or n == 1:
             return n
-        arr = [0]*(n+1)
-        arr[0] = 0
-        arr[1] = 1
+        f1, f2 = 0, 1
         for i in range(2, n+1):
-            arr[i] = arr[i-1] + arr[i-2]
-        return arr[n]
+            ans = f1 + f2
+            f1 = f2
+            f2 = ans
+        return ans
