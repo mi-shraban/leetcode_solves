de538d014ff7aaa629cd8dddcb4edc7a55e6ca76
Md Monowarul Islam <83732674+mi-shraban@users.noreply.github.com>
2025-11-03 01:19:21 +0600

    Time: 21 ms (85.97%) | Memory: 47.6 MB (95.07%) - LeetSync

--- 2169-simple-bank-system/simple-bank-system.py
+++ 2169-simple-bank-system/simple-bank-system.py
@@ -3,18 +3,18 @@ class Bank:
         self.balance = balance
         self.size = len(balance)
     def transfer(self, a1: int, a2: int, money: int) -> bool:
-        if a1-1 >= self.size or a2-1 >= self.size or self.balance[a1-1] < money:
+        if a1-1 >= self.size or a2-1 >= self.size or self.balance[a1 - 1] < money:
             return False
-        self.balance[a1-1] -= money
-        self.balance[a2-1] += money
+        self.balance[a1 - 1] -= money
+        self.balance[a2 - 1] += money
         return True
     def deposit(self, a: int, money: int) -> bool:
         if a-1 >= self.size:
             return False
-        self.balance[a-1] += money
+        self.balance[a - 1] += money
         return True
     def withdraw(self, a: int, money: int) -> bool:
-        if a-1 >= self.size or self.balance[a-1] < money:
+        if a-1 >= self.size or self.balance[a - 1] < money:
             return False
-        self.balance[a-1] -= money
+        self.balance[a - 1] -= money
         return True
