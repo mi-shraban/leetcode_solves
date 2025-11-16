8aefa89f13bc81d2831f642a2247232b4d96df75
Md Monowarul Islam <83732674+mi-shraban@users.noreply.github.com>
2025-11-16 14:39:21 +0600

    Time: 7 ms (94.38%) | Memory: 18.5 MB (16.10%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.py
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.py
@@ -1,13 +1,22 @@
 class Solution:
     def numSub(self, s: str) -> int:
-        ans = 0
+        # ans = 0
+        # mod = 10**9 + 7
+        # n = 0
+        # for x in s:
+        #     if x == '1':
+        #         n += 1
+        #     else:
+        #         ans += ((n * (n + 1)) // 2) % mod
+        #         n = 0
+        # ans += ((n * (n + 1)) // 2) % mod
+        # return ans
         mod = 10**9 + 7
-        n = 0
+        cnt = 0
+        ans = 0
+        s = s.split('0')
         for x in s:
-            if x == '1':
-                n += 1
-            else:
-                ans += ((n * (n + 1)) // 2) % mod
-                n = 0
-        ans += ((n * (n + 1)) // 2) % mod
+            n = len(x)
+            cnt += n * (n + 1)
+        ans = (cnt // 2) % mod
         return ans
