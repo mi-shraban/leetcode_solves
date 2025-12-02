faaab2a65512cac92a61babf9293730b3436b8f8
Md Monowarul Islam <83732674+mi-shraban@users.noreply.github.com>
2025-12-02 11:37:53 +0600

    Time: 99 ms (16.29%) | Memory: 63.5 MB (72.53%) - LeetSync

---
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countTrapezoids(self, points: List[List[int]]) -> int:
+        mod = 10**9+7
+        ht = {y:0 for x, y in points}
+        for x, y in points:
+            ht[y] += 1
+        ans = total = 0
+        for y, cnt in ht.items():
+            lines = cnt * (cnt - 1) // 2
+            ans = (ans + total * lines) % mod
+            total = (total + lines) % mod
+        return ans
