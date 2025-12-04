3a4fbbbac2f0632d67fb31e2a5dbed4c9ae53d6c
Md Monowarul Islam <83732674+mi-shraban@users.noreply.github.com>
2025-12-04 09:46:40 +0600

    Time: 86 ms (67.29%) | Memory: 19.5 MB (6.69%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.py
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.py
@@ -1,28 +1,41 @@
 class Solution:
     def countCollisions(self, directions: str) -> int:
-        ans = 0
-        stack = []
-        for x in directions:
-            if not stack:
-                stack.append(x)
-            else:
-                top = stack[-1]
-                curr = x
-                if top == 'R' and x == 'L':
-                    ans += 2
-                    stack.pop()
-                    curr = 'S'
-                elif top == 'R' and x == 'S':
-                    ans += 1
-                    stack.pop()
-                    curr = 'S'
-                elif top == 'S' and x == 'L':
-                    ans += 1
-                    stack.pop()
-                    curr = 'S'
-                while stack and (stack[-1] == 'R' and curr == 'S'):
-                    ans += 1
-                    stack.pop()
-                stack.append(curr)
-        return ans
-        # stack solve ^^
+        # ans = 0
+        # stack = []
+        # for x in directions:
+        #     if not stack:
+        #         stack.append(x)
+        #     else:
+        #         top = stack[-1]
+        #         curr = x
+        #         if top == 'R' and x == 'L':
+        #             ans += 2
+        #             stack.pop()
+        #             curr = 'S'
+        #         elif top == 'R' and x == 'S':
+        #             ans += 1
+        #             stack.pop()
+        #             curr = 'S'
+        #         elif top == 'S' and x == 'L':
+        #             ans += 1
+        #             stack.pop()
+        #             curr = 'S'
+        #         while stack and (stack[-1] == 'R' and curr == 'S'):
+        #             ans += 1
+        #             stack.pop()
+        #         stack.append(curr)
+        # return ans
+        # # stack solve ^^
+        n = len(directions)
+        if n == 0:
+            return n
+        l, r = 0, n-1
+        while l < r and directions[l] == 'L':
+            l += 1
+        while r > l and directions[r] == 'R':
+            r -= 1
+        print(l, r)
+        if l >= r:
+            return 0
+        return sum([directions[i] != 'S' for i in range(l, r+1)])
+        # Two pointer solution ^^
