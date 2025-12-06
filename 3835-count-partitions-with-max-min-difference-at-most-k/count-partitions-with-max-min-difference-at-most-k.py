cbc39adf0c6f32889b481c456ce572c2dc76ae0c
Md Monowarul Islam <83732674+mi-shraban@users.noreply.github.com>
2025-12-06 10:43:39 +0600

    Time: 393 ms (77.65%) | Memory: 25.5 MB (58.82%) - LeetSync

--- 3835-count-partitions-with-max-min-difference-at-most-k/count-partitions-with-max-min-difference-at-most-k.py
+++ 3835-count-partitions-with-max-min-difference-at-most-k/count-partitions-with-max-min-difference-at-most-k.py
@@ -1,16 +1,22 @@
 class Solution:
     def countPartitions(self, nums: List[int], k: int) -> int:
-        mod = 10**9 + 7
-        n = len(nums)
-        pref = [0, 1]
-        ans = -1
-        j = 0
-        window = SortedList()
-        for i in range(n):
-            window.add(nums[i])
-            while j <= i and window[-1] - window[0] > k:
-                window.remove(nums[j])
-                j += 1
-            ans = (pref[-1] - pref[j] + mod) % mod
-            pref.append((ans + pref[-1]) % mod)
-        return ans
+        l, cnt, mod = 0, 1, 10**9 + 7
+        mxQ, mnQ, dp = deque(), deque(), [cnt]
+        for r, x in enumerate(nums):
+            while mxQ and x > nums[mxQ[-1]]:
+                mxQ.pop()
+            while mnQ and x < nums[mnQ[-1]]:
+                mnQ.pop()
+            mxQ.append(r)
+            mnQ.append(r)
+            while nums[mxQ[0]] - nums[mnQ[0]] > k:
+                cnt -= dp[l]
+                l += 1
+                if l > mnQ[0]:
+                    mnQ.popleft()
+                if l > mxQ[0]:
+                    mxQ.popleft()
+            dp.append(cnt)
+            cnt *= 2
+            cnt %= mod
+        return dp[-1] % mod
