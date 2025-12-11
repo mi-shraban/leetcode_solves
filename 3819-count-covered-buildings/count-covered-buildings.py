1395b17dba60f9f850ca941f090bf66601ccaa26
Md Monowarul Islam <83732674+mi-shraban@users.noreply.github.com>
2025-12-11 11:34:53 +0600

    Time: 151 ms (100.00%) | Memory: 56.1 MB (86.55%) - LeetSync

--- 3819-count-covered-buildings/count-covered-buildings.py
+++ 3819-count-covered-buildings/count-covered-buildings.py
@@ -1,14 +1,18 @@
 class Solution:
     def countCoveredBuildings(self, n: int, buildings: List[List[int]]) -> int:
-        rt = [0] * (n + 1)
-        rb = [n + 1] * (n + 1)
-        cl = [0] * (n + 1)
-        cr = [n + 1] * (n + 1)
+        rt = [0] * (n + 1)      # row top
+        rb = [n + 1] * (n + 1)  # row bottom
+        cl = [0] * (n + 1)      # column left
+        cr = [n + 1] * (n + 1)  # column right
         for x, y in buildings:
-            rt[y] = max(rt[y], x)
-            rb[y] = min(rb[y], x)
-            cl[x] = max(cl[x], y)
-            cr[x] = min(cr[x], y)
+            if x > rt[y]:
+                rt[y] = x
+            if x < rb[y]:
+                rb[y] = x
+            if y > cl[x]:
+                cl[x] = y
+            if y < cr[x]:
+                cr[x] = y
         ans = 0
         for x, y in buildings:
             if rb[y] < x < rt[y] and cr[x] < y < cl[x]:
