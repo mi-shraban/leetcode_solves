9680dc3015fac90a3f77d206ffc1920ddc7cd719
Md Monowarul Islam <83732674+mi-shraban@users.noreply.github.com>
2025-12-15 11:43:17 +0600

    Time: 67 ms (33.21%) | Memory: 29.6 MB (96.64%) - LeetSync

--- 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.py
+++ 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.py
@@ -1,6 +1,7 @@
 class Solution:
     def getDescentPeriods(self, prices: List[int]) -> int:
-        ans, cnt = 1, 1
+        ans = 1
+        cnt = 1
         for i in range(len(prices) - 1):
             if prices[i] - prices[i + 1] == 1:
                 cnt += 1
